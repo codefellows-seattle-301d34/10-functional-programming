72a932813c0c0bcce99c60f671137c7c4eb8131b
Amy Cohen <anbcohen@gmail.com>
2018-05-20 14:10:18 -0700

    going through and adding app to the places I believe need it. Sadly my internet access is down so I feel so out of luck with this assignment.

--- amy-cohen/public/scripts/article.js
+++ amy-cohen/public/scripts/article.js
@@ -3,14 +3,15 @@ var app = app || {};
 
 
 //because of all the app.Article(s) used, this needs to be an IIFE, but can rawDataObj be the module since that is just potato?  Also, wouldn't ALL Article references need to be app.Article then so it can be revealed correctly?
-function Article(rawDataObj) {
+(function Article(rawDataObj) {
   // REVIEW: In Lab 8, we explored a lot of new functionality going on here. Let's re-examine the concept of context. Normally, "this" inside of a constructor function refers to the newly instantiated object. However, in the function we're passing to forEach, "this" would normally refer to "undefined" in strict mode. As a result, we had to pass a second argument to forEach to make sure our "this" was still referring to our instantiated object. One of the primary purposes of lexical arrow functions, besides cleaning up syntax to use fewer lines of code, is to also preserve context. That means that when you declare a function using lexical arrows, "this" inside the function will still be the same "this" as it was outside the function. As a result, we no longer have to pass in the optional "this" argument to forEach!
   Object.keys(rawDataObj).forEach(key => this[key] = rawDataObj[key]);
-};
+})(app);
 
-Article.all = [];
+//if Article above is wrapped as an IIFE, then this will need to be called app.Article.all
+app.Article.all = [];
 
-Article.prototype.toHtml = function() {
+app.Article.prototype.toHtml = function() {
   var template = Handlebars.compile($('#article-template').text());
 
   this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
@@ -20,7 +21,7 @@ Article.prototype.toHtml = function() {
   return template(this);
 };
 
-Article.loadAll = articleData => {
+app.Article.loadAll = articleData => {
   articleData.sort((a,b) => (new Date(b.publishedOn)) - (new Date(a.publishedOn)))
 
   /* OLD forEach():
@@ -29,7 +30,7 @@ Article.loadAll = articleData => {
 
 };
 
-Article.fetchAll = callback => {
+app.Article.fetchAll = callback => {
   $.get('/articles')
     .then(results => {
       Article.loadAll(results);
@@ -38,17 +39,17 @@ Article.fetchAll = callback => {
 };
 
 // Hint: What property of an individual instance contains the main text of the article?
-Article.numWordsAll = () => {
+app.Article.numWordsAll = () => {
   return Article.body.map().reduce()
 };
 
 // Hint: Make sure to return an array and avoid duplicates.
-Article.allAuthors = () => {
+app.Article.allAuthors = () => {
   return Article.all.map().reduce();
 };
 
 
-Article.numWordsByAuthor = () => {
+app.Article.numWordsByAuthor = () => {
   return Article.allAuthors().map(author => 
     return {
       name: Article.author,
@@ -57,7 +58,7 @@ Article.numWordsByAuthor = () => {
     });
 };
 
-Article.truncateTable = callback => {
+app.Article.truncateTable = callback => {
   $.ajax({
     url: '/articles',
     method: 'DELETE',
@@ -67,14 +68,14 @@ Article.truncateTable = callback => {
     .then(callback);
 };
 
-Article.prototype.insertRecord = function(callback) {
+app.Article.prototype.insertRecord = function(callback) {
   // REVIEW: Why can't we use an arrow function here for .insertRecord()?
   $.post('/articles', {author: this.author, authorUrl: this.authorUrl, body: this.body, category: this.category, publishedOn: this.publishedOn, title: this.title})
     .then(console.log)
     .then(callback);
 };
 
-Article.prototype.deleteRecord = function(callback) {
+app.Article.prototype.deleteRecord = function(callback) {
   $.ajax({
     url: `/articles/${this.article_id}`,
     method: 'DELETE'
@@ -83,7 +84,7 @@ Article.prototype.deleteRecord = function(callback) {
     .then(callback);
 };
 
-Article.prototype.updateRecord = function(callback) {
+app.Article.prototype.updateRecord = function(callback) {
   $.ajax({
     url: `/articles/${this.article_id}`,
     method: 'PUT',
