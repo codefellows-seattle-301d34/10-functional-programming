10ec647f418dfbf079a623e7d15bfeb244df7733
Karina Amador <akarina.at@gmail.com>
2018-05-20 17:22:07 -0700

    export, loadall.map, iife

--- karina-lab10/public/scripts/article.js
+++ karina-lab10/public/scripts/article.js
@@ -1,6 +1,8 @@
 'use strict';
 var app = app || {};
 
+(function (module){
+
 function Article(rawDataObj) {
   // REVIEW: In Lab 8, we explored a lot of new functionality going on here. Let's re-examine the concept of context. Normally, "this" inside of a constructor function refers to the newly instantiated object. However, in the function we're passing to forEach, "this" would normally refer to "undefined" in strict mode. As a result, we had to pass a second argument to forEach to make sure our "this" was still referring to our instantiated object. One of the primary purposes of lexical arrow functions, besides cleaning up syntax to use fewer lines of code, is to also preserve context. That means that when you declare a function using lexical arrows, "this" inside the function will still be the same "this" as it was outside the function. As a result, we no longer have to pass in the optional "this" argument to forEach!
   Object.keys(rawDataObj).forEach(key => this[key] = rawDataObj[key]);
@@ -24,6 +26,9 @@ Article.loadAll = articleData => {
   /* OLD forEach():
   articleData.forEach(articleObject => Article.all.push(new Article(articleObject)));
   */
+//  Article.all.map(articleObject => articleData(new Article));
+
+Article.all = articleData.map(articleObject => ( new Article( articleObject )));
 
 };
 
@@ -31,13 +36,13 @@ Article.fetchAll = callback => {
   $.get('/articles')
     .then(results => {
       Article.loadAll(results);
-      callback();
+      callback(articleData);
     })
 };
 
 // Hint: What property of an individual instance contains the main text of the article?
 Article.numWordsAll = () => {
-  return Article.all.map().reduce()
+  return Article.all.map(Article => Article.length).reduce(Article.body)
 };
 
 // Hint: Make sure to return an array and avoid duplicates.
@@ -97,4 +102,6 @@ Article.prototype.updateRecord = function(callback) {
   })
     .then(console.log)
     .then(callback);
-};
+}
+module.Article = Article;
+})(app);
